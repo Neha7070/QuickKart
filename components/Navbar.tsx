6585a0232e28180af883d6a3f179631f50a1137b
Neha Kumari <nehakumari121@gmail.com>
2023-07-16 01:28:40 +0530

    login pade added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 export default function Navbar(){
    return( 
      <>
@@ -13,9 +14,9 @@ export default function Navbar(){
       <a className="mr-5 hover:text-gray-900">Headphones</a>
       <a className="mr-5 hover:text-gray-900">Contact</a>
     </nav>
-    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
+    <Link href={"/login"} className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
       
-    </button>
+    </Link>
   </div>
 </header>
      </>
