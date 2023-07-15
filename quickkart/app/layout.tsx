1309c703df616275e8402ece2c0516ecc2894f04
Neha Kumari <nehakumari121@gmail.com>
2023-07-16 00:13:59 +0530

    Navbar Error solved

--- quickkart/app/layout.tsx
+++ quickkart/app/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar"
+import Navbar from "../components/Navbar"
 import "../styles/globals.css"
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
