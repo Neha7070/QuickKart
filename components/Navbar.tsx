02e9b54e6d8f44b5f78ea044af189e4e5d3dd61b
Neha Kumari <nehakumari121@gmail.com>
2023-07-16 01:10:04 +0530

    nav bar component completed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,23 @@
 export default function Navbar(){
    return( 
      <>
-      <div className="bg-red-500 h-10">
-        Navbar
-      </div>
+      <header className="text-gray-600 body-font">
+  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      
+      <span className="ml-3 text-xl">QuickKart</span>
+    </a>
+    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+      <a className="mr-5 hover:text-gray-900">SmartPhones</a>
+      <a className="mr-5 hover:text-gray-900">Laptops</a>
+      <a className="mr-5 hover:text-gray-900">Headphones</a>
+      <a className="mr-5 hover:text-gray-900">Contact</a>
+    </nav>
+    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
+      
+    </button>
+  </div>
+</header>
      </>
    )
 }
